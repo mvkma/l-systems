739dbf42eefcd6b466d8aa4ffe6a00438d82252e
Matthias Volk <mv@infrablue.org>
2025-02-24 17:11:33 +0100

    Parse parameters to RPN and evaluate

--- src/language.js
+++ src/language.js
@@ -1,6 +1,128 @@
+const operators = {
+    "+": {
+        prec: 1,
+        args: 2,
+        func: (x, y) => x + y,
+    },
+    "-": {
+        prec: 1,
+        args: 2,
+        func: (x, y) => x - y,
+    },
+    "*": {
+        prec: 2,
+        args: 2,
+        func: (x, y) => x * y,
+    },
+    "/": {
+        prec: 2,
+        args: 2,
+        func: (x, y) => x / y,
+    },
+}
+
+/**
+ * Evaluate RPN
+ *
+ * @param {Array<string>} tokens
+ * @param {Object<string,number>} bindings
+ *
+ * @returns {number}
+ */
+function evalRPN(tokens, bindings) {
+    let op;
+    let val;
+    let args;
+    const stack = [];
+
+    for (const t of tokens) {
+        op = operators[t];
+
+        if (op === undefined) {
+            val = bindings[t];
+
+            if (val === undefined) {
+                throw new Error(`Missing binding for variable '${t}'`);
+            } else {
+                stack.push(val);
+            }
+        } else {
+            args = [];
+            for (let i = 0; i < op.args; i++) {
+                args.push(stack.pop());
+            }
+            stack.push(op.func(...args.reverse()));
+        }
+    }
+
+    if (stack.length > 1) {
+        throw new Error("Missing operator");
+    }
+
+    return stack.pop();
+}
+
+/**
+ * Shunting yard algorithm
+ * https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+ *
+ * @param {Array<string>} tokens
+ *
+ * @returns {Array<string>}
+ */
+function shuntingYard(tokens) {
+    const stack = [];
+    const result = [];
+
+    for (const t of tokens) {
+        if (operators[t] !== undefined) {
+            while (stack.length > 0 &&
+                   stack.at(-1) !== "(" &&
+                   operators[stack.at(-1)].prec >= operators[t].prec) {
+                result.push(stack.pop());
+            }
+            stack.push(t);
+        } else if (t === "(") {
+            stack.push(t);
+        } else if (t === ")") {
+            while (stack.length > 0 && stack.at(-1) !== "(") {
+                result.push(stack.pop());
+            }
+
+            if (stack.length > 0 && stack.at(-1) === "(") {
+                stack.pop();
+            } else {
+                throw new Error("Parentheses error");
+            }
+        } else {
+            result.push(t);
+        }
+    }
+
+    while (stack.length > 0) {
+        if (stack.at(-1) === "(") {
+            throw new Error("Parentheses error");
+        } else {
+            result.push(stack.pop());
+        }
+    }
+
+    return result;
+}
+
 function symb(s) {
-    return function(...params) {
-        return { "symbol": s, "parameters": params };
+    return function(parameters, bindings) {
+        const values = {};
+
+        for (const [k, rpn] of Object.entries(parameters)) {
+            values[k] = evalRPN(rpn, bindings);
+        }
+
+        return {
+            "symbol": s,
+            "parameters": parameters,
+            "values": values,
+        };
     };
 }
 
@@ -14,8 +136,12 @@ function evolve(axiom, rules, level) {
         console.log(n, stateToString(state));
         next = [];
         for (const s of state) {
-            rule = rules[s["symbol"]] || symbols[s["symbol"]];
-            next = next.concat(rule(...s["parameters"]));
+            rule = rules[s["symbol"]];
+            if (rule === undefined) {
+                next = next.concat([s]);
+            } else {
+                next = next.concat(rule(s["values"]));
+            }
         }
         state = next;
         n++;
@@ -31,7 +157,7 @@ function stateToString(state) {
     for (const s of state) {
         out += s["symbol"];
         out += "(";
-        out += s["parameters"].toString();
+        out += JSON.stringify(s["values"]);
         out += ")";
     }
 
@@ -51,24 +177,33 @@ const symbols = {
     "-": symb("-"),
 };
 
-const axiom = symbols["F"](1.0);
+const axiom = symbols["F"]({ x: ["x"] }, {x: 1.0});
 
 const rules = {
-    "F": (x) => [
-        symbols["F"](x * consts["p"]),
-        symbols["+"](),
-        symbols["F"](x * consts["h"]),
-        symbols["-"](),
-        symbols["-"](),
-        symbols["F"](x * consts["h"]),
-        symbols["+"](),
-        symbols["F"](x * consts["q"]),
+    "F": (bindings) => [
+        symbols["F"]({ x: ["x", "p", "*"] }, {...bindings, ...consts}),
+        symbols["+"]({}, {}),
+        symbols["F"]({ x: ["x", "h", "*"] }, {...bindings, ...consts}),
+        symbols["-"]({}, {}),
+        symbols["-"]({}, {}),
+        symbols["F"]({ x: ["x", "h", "*"] }, {...bindings, ...consts}),
+        symbols["+"]({}, {}),
+        symbols["F"]({ x: ["x", "q", "*"] }, {...bindings, ...consts}),
     ],
 };
 
 function runLanguage() {
-    console.log(rules);
-    console.log(symbols);
+    // const expr = "x + y * z - a / ( a + b )";
+    // const rpn = shuntingYard(expr.split(" "));
+    // const bindings = { x: 1.0, y: -5.0, z: 0.6, a: 23.5, b: -15.8 };
+    // const res = evalRPN(rpn, bindings);
+
+    // console.log(expr);
+    // console.log(rpn);
+    // console.log(bindings);
+    // console.log(res);
+
+    console.log(consts);
     const state = evolve(axiom, rules, 2);
 }
 
